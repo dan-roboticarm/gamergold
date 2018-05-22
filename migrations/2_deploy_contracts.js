17d0523ffd704728dee70faa8af8644eac3acff0
resourcehog <TravisGRivera@gmail.com>
2018-05-22 13:22:38 -0700

    updated dependencies

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,17 +11,15 @@ module.exports = function(deployer,network,accounts)
     var tokenwallet = companyID;
     var GG;
     var ICO;
-    
+
     deployer.deploy(GamerGold,companyID).then(function(){
         var starttime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 300000000;
         var endtime = starttime + 1000000;
         GG = GamerGold;
         return deployer.deploy(GamerGoldICO, GamerGold.address, tokenwallet, rate, goal, tokenwallet, starttime, endtime).then(function()
         {
-            
             console.log("gamergold object: " + GamerGold);
             GG.at(GG.address).approve(GamerGoldICO.address,16000000000);
         });
     });
-
 }
--- test/TestICO.sol
+++ test/TestICO.sol
@@ -9,10 +9,11 @@ contract TestICO{
     GamerGold gg = GamerGold(DeployedAddresses.GamerGold());
     GamerGoldICO ico = GamerGoldICO(DeployedAddresses.GamerGoldICO());
     
+    //might have to turn this into a javascript test instead of a contract based test so that the test can access the address
+    //for now the companyid is manually entered into the test.
     function testAllowance() public {
         uint256 expected = 16000000000;
-        uint256 allowance = gg.allowance(0x442152AB2Aa10F46D2Acd18D4275d08C2C6E537B,ico);
+        uint256 allowance = gg.allowance(0x442152AB2Aa10F46D2Acd18D4275d08C2C6E537B,ico); //TODO: this should be the first address in the wallet 
         Assert.equal(allowance,expected,"allowance was not given to the ico.");
     }
-
 }
